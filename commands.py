6d8e72222697f124166feed555ba4c0ac2cb9430
Vas Vasiliadis <vas@exordium.com>
2021-11-14 02:21:38 -0600

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,27 +1,3 @@
-# Edit ~/myportal/myportal/settings.py
-
-# Paste your registered client ID and secret
-SOCIAL_AUTH_GLOBUS_KEY = 'YOUR_CLIENT_ID'
-SOCIAL_AUTH_GLOBUS_SECRET = 'YOUR_SECRET'
-
-# Add your server to the ALLOWED_HOSTS list
-ALLOWED_HOSTS = [
-    'scN.globusdemo.org',
-]
-
-# Add django_extensions to the INSTALLED_APPS list
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'globus_portal_framework',
-    'social_django',
-    'django_extensions',
-]
-
 # Copy static resources
 python manage collectstatic
 
