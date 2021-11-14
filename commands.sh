1277465cf2732b774fce40329ef25840668fe407
Vas Vasiliadis <vas@exordium.com>
2021-11-14 02:31:48 -0600

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 # Copy static resources
-python manage collectstatic
+python manage.py collectstatic
 
 # Run the portal server
 python manage.py runserver_plus 0.0.0.0:8443 --cert-file scN.globusdemo.org.cert --key-file scN.globusdemo.org.key
