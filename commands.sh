8ccc90fe18af92efea83d61613b87358a8365871
Vas Vasiliadis <vas@exordium.com>
2022-04-05 00:26:47 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,26 @@
+# Activate the Python environment
+source ~/.portal/bin/activate
+
+# Install dependent libs
+pip install django-extensions Werkzeug pyopenssl cookiecutter
+
+# Point cookiecutter to the Globus portal repository
+cookiecutter https://github.com/globus/cookiecutter-django-globus-app
+
+# MOve to the portal base dir
+cd ./<project_slug>
+
+# Install dependent libs
+pip install -r requirements.txt
+
 # Enable social_django and django_extensions apps
 python manage.py migrate
 
 # Copy static resources
 python manage.py collectstatic
 
-# Run the portal server
-python manage.py runserver_plus 0.0.0.0:8443 --cert-file scN.globusdemo.org.cert --key-file scN.globusdemo.org.key
+# Run the Globus portal
+python manage.py runserver_plus 0.0.0.0:8443 \
+--cert-file /opt/ssl/globusdemo.org.cert \
+--key-file /opt/ssl/globusdemo.org.key \
+--keep-meta-shutdown
