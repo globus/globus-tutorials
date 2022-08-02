38313df9fb75fdacf99e56cf8a4e9b88cdfbfafd
Vas Vasiliadis <vas@exordium.com>
2022-08-02 00:41:15 -0600

    Update flows-commands.sh

--- flows-commands.sh
+++ flows-commands.sh
@@ -17,8 +17,8 @@ cd globusconnectpersonal-3.1.6
 globus login   # optional
 globus endpoint search 'RMACC22'
 
-# Edit simple_script.py; modify "source_id" and "remote_path"
-# When triggered, this Globus Flow will move file to this collection/path
+# For Flow 1: Transfer and Share ----
+# Edit simple_script.py; modify "source_id" and "destination_base_path"
 cd ~/globus-flows-trigger-examples
 vi ~/globus-flows-trigger-examples/trigger_transfer_share_flow.py
 
@@ -30,17 +30,22 @@ destination_base_path = '/automation-tutorial/YOUR_NAME/'  # on line 28
 source ~/.trigger/bin/activate
 
 # Run the trigger script
+# When triggered, this Globus Flow will move file to the guest collection/path and add sharing permissions
 cd ~/globus-flows-trigger-examples
 ./trigger_transfer_share_flow.py --watchdir `pwd`/testData --patterns .done
 
-# Edit trigger_transfer_publish_flow.py; modify "flow_id", "source_id", "remote_path" and "search_index"
-# When triggered, this Globus Flow will push file metadata to the index we created earlier 
+# For Flow 2: Transfer and Publish ---- 
+# Edit trigger_transfer_publish_flow.py; modify "flow_id", "source_id", "destination_base_path" and "search_index"
+cd ~/globus-flows-trigger-examples
+vi ~/globus-flows-trigger-examples/trigger_transfer_share_flow.py
+
 flow_id = 'YOUR_FLOW_ID_FROM_NOTEBOOK'  # on line 16
 source_id = 'YOUR_GCP_ENDPOINT_ID'  # on line 21
 destination_base_path = '/automation-tutorial/YOUR_NAME/'  # on line 30
 search_index = 'YOUR_GLOBUS_SEARCH_INDEX_ID_FROM_NOTEBOOK'   # on line 37
 
-# Run the script to ingest search metadata
+# Run the trigger script
+# When triggered, this Globus Flow will push metadata to the index we created earlier
 cd ~/globus-flows-trigger-examples
 ./trigger_transfer_publish_flow.py --watchdir `pwd`/testData --patterns .done
 
